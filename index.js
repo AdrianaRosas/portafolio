105fd838acca23cf6e0c33df1a68dc61c90df29a
AdrianaRosas <salamipizza8909@gmail.com>
2019-10-31 00:06:07 -0600

    Diseño y animaciones

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 // SOCIAL PANEL JS
 const floating_btn = document.querySelector('.floating-btn');
 const close_btn = document.querySelector('.close-btn');
@@ -7,7 +9,21 @@ floating_btn.addEventListener('click', () => {
 	social_panel_container.classList.toggle('visible')
 });
 
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
 
+// Scroll
+const proyectsTittle = document.querySelector('.proyects-tittle');
+
+document.addEventListener('scroll', ()=>{
+let scrollY = window.scrollY + (window.innerHeight / 3);	
+   scrollY >= proyectsTittle.offsetTop ? proyectsTittle.classList.add('anim-typewriter') : false;
+})
+
+const lineOne = document.querySelector('.line-1');
+
+document.addEventListener('scroll', ()=>{
+let scrollY = window.scrollY + (window.innerHeight / 3);	
+   scrollY >= lineOne.offsetTop ? lineOne.classList.add('anim-typewriter') : false;
+})
+// OffsetTop mide la posición de arriba a abajo del elemento con respecto al documento
+// innerHeight mide la altura de la pantalla
+// scrollY es una propiedad para detectar la posición vertical de la ventana del navegador donde etamos visualizando la página
